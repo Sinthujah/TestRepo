40186b1f15b97c711fe2f42a5e9320056fb3efe5
Sinthujah <79849431+Sinthujah@users.noreply.github.com>
2021-07-14 15:20:39 +0530

    added a new task

--- XLRelease/sonic/test template.groovy
+++ XLRelease/sonic/test template.groovy
@@ -1,6 +1,6 @@
 // Exported from:        http://qe-centos-ha2:5516/#/templates/Folder10f432c5e9a048db8bb13c25abebacd0-Release75fb1a003c1c456f9bb34fe683a6e32a/code
 // Release version:      10.2.0-708.113
-// Date created:         Wed Jul 14 11:47:41 CEST 2021
+// Date created:         Wed Jul 14 11:49:41 CEST 2021
 
 xlr {
   template('test template') {
@@ -12,6 +12,9 @@ xlr {
         tasks {
           manual('task 1') {
             description 'this is task 1 for manual task'
+          }
+          manual('task 2') {
+            
           }
         }
       }
